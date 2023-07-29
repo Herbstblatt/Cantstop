de0782cf8a84229857313516a96f2bb4aa65eea0
Rusy-Sgt <35418128+Rusy-Sgt@users.noreply.github.com>
2023-07-29 17:03:34 +0300

    Update __init__.py

--- bot/games/cantstop/__init__.py
+++ bot/games/cantstop/__init__.py
@@ -7,5 +7,6 @@ async def setup(_):
 
 async def start(room: Room):
     assert room.view is not None and room.view.active_interaction is not None
-    view = Game(players=room.participants)
+    view = Game(room)
     await room.view.active_interaction.followup.send(content=view.content, view=view)
+    view.last_reminder = await room.view.message.channel.send(f'Your turn, {view.current_player.mention}!')
